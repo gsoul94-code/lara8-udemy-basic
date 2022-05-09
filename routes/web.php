ea7209ded7fd3cfde8fee41a1fdbff50403202ba
gsoul94 code <gsoul94.code@gmail.com>
2022-05-09 14:40:26 +0800

    Create Middleware for check age to restrict page

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/check-age', function () {
+    echo "This page has a limited access by checkAge Middleware.";
+    echo "<br>";
+    echo "If you in this page, your age over 18";
+})->middleware('checkAge');
+
+Route::get('/check-age-restricted', function () {
+    echo "If you in this page, that means your age is under 18";
+});
+
 Route::get('/about', [AboutController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
